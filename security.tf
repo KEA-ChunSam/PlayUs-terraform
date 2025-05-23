4fb47d79ff0d692252791ac8bebcc0d0ce3651dd
Kim-Yukyung <yukyung116@gmail.com>
2025-05-23 20:41:04 +0900

    ⚡️ Bastion SSH 모든 포트 허용

--- security.tf
+++ security.tf
@@ -11,7 +11,7 @@ resource "openstack_networking_secgroup_rule_v2" "bastion-ssh-sg-rule" {
   protocol          = "tcp"
   port_range_min    = 22
   port_range_max    = 22
-  remote_ip_prefix  = "${openstack_networking_floatingip_v2.bastion_fip.address}/32"
+  remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = openstack_networking_secgroup_v2.bastion_sg.id
 }
 
