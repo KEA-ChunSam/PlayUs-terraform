9dbbddf94d8fc7742d3bdfbaebae154b0adb5b4b
Kim-Yukyung <yukyung116@gmail.com>
2025-07-23 22:19:54 +0900

    🚚 변수명 변경

--- terraform.tf
+++ terraform.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "openstack" {
   auth_url                      = var.auth_url
-  application_credential_id     = var.application_credential_id
-  application_credential_secret = var.application_credential_secret
+  application_credential_id     = var.credential_id
+  application_credential_secret = var.credential_secret
   region                        = var.region
 }
