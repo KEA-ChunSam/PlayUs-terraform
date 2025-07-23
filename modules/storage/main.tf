6620e9780e74fc8f80df32857237887f6d8c378b
Kim-Yukyung <yukyung116@gmail.com>
2025-07-23 21:41:58 +0900

    ♻️  terraform fmt 적용

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -9,4 +9,4 @@ terraform {
 
 resource "openstack_objectstorage_container_v1" "s3_bucket" {
   name = var.s3_bucket_name
-} 
+}
--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -1,4 +1,4 @@
 output "s3_bucket_name" {
   description = "S3 버킷 이름"
   value       = openstack_objectstorage_container_v1.s3_bucket.name
-} 
+}
